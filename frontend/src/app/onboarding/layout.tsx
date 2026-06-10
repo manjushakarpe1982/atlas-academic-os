25df8b2f8221a867f01af25d219f1be226571719
Manjusha karpe <32287237+manjushakarpe1982@users.noreply.github.com>
2026-06-10 10:22:32 +0530

    remove exting file

--- frontend/src/app/onboarding/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingProvider } from './_components/OnboardingContext';
-
-export default function OnboardingSegmentLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <OnboardingProvider>{children}</OnboardingProvider>;
-}
